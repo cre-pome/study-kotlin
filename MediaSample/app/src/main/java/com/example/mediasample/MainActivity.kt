0e7ce70882b8692b776692ada11e258dddbf1fa1
cre-pome <pometaka91@gmail.com>
2021-06-14 01:55:01 +0900

    kotlinで勉強したレポジトリ(2章分)

---
+++ MediaSample/app/src/main/java/com/example/mediasample/MainActivity.kt
@@ -0,0 +1,169 @@
+package com.example.mediasample
+
+import android.media.MediaPlayer
+import android.net.Uri
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Button
+import android.widget.CompoundButton
+import android.widget.Switch
+import kotlinx.android.synthetic.main.activity_main.*
+import java.io.IOException
+import java.lang.IllegalArgumentException
+
+class MainActivity : AppCompatActivity() {
+
+    // メディアプレーヤーフィールド
+    private var _player:MediaPlayer? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // フィールドのメディアプレーヤーオブジェクトを生成。
+        _player = MediaPlayer()
+
+        // 音声ファイルのURI文字列を作成。
+        val mediaFIleStr = "android.resource://${packageName}/${R.raw.mountain_stream}"
+        Log.i("mediasanmpme", "create mediaFIleStr :" + mediaFIleStr )
+        // 音声ファイルのURI文字列を元にURIオブジェクトを生成。
+        val mediaFileUri = Uri.parse(mediaFIleStr)
+
+        try{
+            // メディアプレーヤーに音声ファイルを指定
+            _player?.setDataSource(applicationContext, mediaFileUri)
+
+            // 非同期でのメディア再生準備が完了した際のリス名を設定
+            _player?.setOnPreparedListener (PlayerPreparedListener() )
+
+            // メディア再生が終了した際のリスナを設定
+            _player?.setOnCompletionListener (PlayerCompletionListener())
+
+            // 非同期でメディア再生を準備
+            _player?.prepareAsync()
+
+        }catch (ex:IllegalArgumentException){
+            Log.e("MediaSample", "メディアプレーヤー準備時の例外発生")
+        }
+        catch(ex:IOException) {
+            Log.e("MediaSample", "メディアプレーヤー準備時の例外発生")
+        }
+
+        // スイッチを取得
+        val loopSwitch = findViewById<Switch>(R.id.swLoop)
+
+        // スイッチにリスナを設定
+        loopSwitch.setOnCheckedChangeListener(LoopSwitchChangedListener())
+    }
+
+    // プレーヤーの再生準備が整った時のリスナクラス
+    private inner class PlayerPreparedListener:MediaPlayer.OnPreparedListener{
+        override fun onPrepared(p0: MediaPlayer?) {
+            // 各ボタンをタップ可能に設定。
+            val btPlay = findViewById<Button>(R.id.btPlay)
+            btPlay.isEnabled = true
+            val btBack = findViewById<Button>(R.id.btBack)
+            btBack.isEnabled = true
+            val btForward = findViewById<Button>(R.id.btForward)
+            btForward.isEnabled = true
+        }
+    }
+
+    // 再生が終了した時のリスナクラス
+    private inner class PlayerCompletionListener:MediaPlayer.OnCompletionListener{
+
+        override fun onCompletion(p0: MediaPlayer) {
+            _player?.let{
+                // ループ設定がされていなければ
+                if(!it.isLooping){
+                    // 再生ボタンのラベルを「再生」に設定。
+                    val btPlay = findViewById<Button>(R.id.btPlay)
+                    btPlay.setText(R.string.bt_play_play)
+                }
+            }
+            
+        }
+    }
+
+    fun onPlayButtonClick(view: View){
+        // フィールドのプレーヤーがnullじゃなかったら
+        _player?.let{
+            // 再生ボタンを取得
+            val btPlayer = findViewById<Button>(R.id.btPlay)
+            // プレーヤーが再生中だったら
+            if(it.isPlaying){
+                // プレーヤーを一時停止
+                it.pause()
+
+                // 再生ボタンのラベルを「再生」に設定
+                btPlayer.setText(R.string.bt_play_play)
+
+            }
+            // プレーヤーが再生中じゃなかったら
+            else {
+                // プレーヤーを再生
+                it.start()
+
+                // 再生ボタンのラベルを「一時停止」に設定
+                btPlay.setText(R.string.bt_play_pause)
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        // 親クラスのメソッド呼び出し
+        super.onDestroy()
+
+        // フィールドのプレーヤーがnullじゃなかったら
+        _player?.let{
+            // プレーヤーが再生中だったら
+            if(it.isPlaying){
+                // プレーヤーを停止
+                it.pause()
+            }
+            // プレーヤーを解放
+            it.release()
+
+            // プレーヤー用フィールドをnullに
+            _player = null
+
+        }
+    }
+
+    fun onBackButtonClick(view:View){
+        // 再生位置を先頭に変更
+        _player?.seekTo(0)
+    }
+
+    fun onForwardButtonClick(view:View){
+
+
+        // フィールドのプレーヤーがnullじゃなかったら
+        _player?.let{
+            // 現在再生中のメディアファイルの長さを取得
+            val duration = it.duration
+
+            // 再生位置を終端に変更。
+            it.seekTo(duration)
+
+            // 再生中でないなら
+            if(!it.isPlaying){
+                //　再生を開始
+                it.start()
+            }
+
+        }
+
+    }
+
+
+    private inner class LoopSwitchChangedListener:CompoundButton.OnCheckedChangeListener{
+        override fun onCheckedChanged(p0: CompoundButton?, isChecked: Boolean) {
+            // ループするかどうかを設定
+            _player?.isLooping = isChecked
+        }
+
+    }
+}
